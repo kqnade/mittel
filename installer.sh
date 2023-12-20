d2fa46ef95a3a808aa58950dedb450d03586330f
kqnade <git@k4na.de>
2023-12-20 15:21:13 +0900

    Block dup install

--- installer.sh
+++ installer.sh
@@ -37,6 +37,15 @@ fi
 # download
 echo "Downloading Mittel..."
 echo ""
+if [[ -d $HOME/.mtl ]]; then
+  echo "Mittel already installed!"
+  echo "Updating..."
+  cd $HOME/.mtl
+  git pull
+  cd $HOME
+  echo "Mittel updated!"
+  exit 1
+fi
 git clone -b $MTL_BRANCH https://github.com/kqnade/mittel.git $HOME/.mtl
 
 
@@ -44,15 +53,82 @@ git clone -b $MTL_BRANCH https://github.com/kqnade/mittel.git $HOME/.mtl
 # add path to .bashrc or .zshrc or something
 read -p "Do you want to add Mittel to your PATH? (y/n) " -n 1 -r
 echo ""
+SHELLNAME=$(basename $SHELL)
 if [[ $REPLY =~ ^[Yy]$ ]]; then
   echo "Adding Mittel to PATH..."
-  if [[ -f "$HOME/.bashrc" ]]; then
-    echo "export PATH=\$PATH:$HOME/.mtl/bin" >> $HOME/.bashrc
+  if [[ $SHELLNAME == "bash" ]]; then
+    duppath=$(grep -c "export PATH=\$PATH:$HOME/.mtl/bin" $HOME/.bashrc)
+    if [[ $duppath -gt 0 ]]; then
+      echo "Mittel already added to PATH!"
+      echo "Mittel not added to PATH."
+      echo "You can add it manually by adding the following line to your .bashrc or .zshrc:"
+      echo "  export PATH=\$PATH:$HOME/.mtl/bin"
+    else
+      echo "export PATH=\$PATH:$HOME/.mtl/bin" >> $HOME/.bashrc
+      source $HOME/.bashrc
+    fi
   fi
-  if [[ -f "$HOME/.zshrc" ]]; then
-    echo "export PATH=\$PATH:$HOME/.mtl/bin" >> $HOME/.zshrc
+  if [[ $SHELLNAME == "zsh" ]]; then
+    duppath=$(grep -c "export PATH=\$PATH:$HOME/.mtl/bin" $HOME/.zshrc)
+    if [[ $duppath -gt 0 ]]; then
+      echo "Mittel already added to PATH!"
+      echo "Mittel not added to PATH."
+      echo "You can add it manually by adding the following line to your .bashrc or .zshrc:"
+      echo "  export PATH=\$PATH:$HOME/.mtl/bin"
+    else
+      echo "export PATH=\$PATH:$HOME/.mtl/bin" >> $HOME/.zshrc
+      source $HOME/.zshrc
+    fi
+  fi
+  if [[ $SHELLNAME == "fish" ]]; then
+    duppath=$(grep -c "export PATH=\$PATH:$HOME/.mtl/bin" $HOME/.config/fish/config.fish)
+    if [[ $duppath -gt 0 ]]; then
+      echo "Mittel already added to PATH!"
+      echo "Mittel not added to PATH."
+      echo "You can add it manually by adding the following line to your .bashrc or .zshrc:"
+      echo "  export PATH=\$PATH:$HOME/.mtl/bin"
+    else
+      echo "set -U fish_user_paths $HOME/.mtl/bin \$fish_user_paths" >> $HOME/.config/fish/config.fish
+      source $HOME/.config/fish/config.fish
+    fi
+  else
+    echo "Can't set PATH automatically."
+    echo "Maybe you use a shell that is not bash, zsh or fish."
+    echo "Look like you use $SHELLNAME."
+    read -p "Setup custom path? (y/n) " -n 1 -r
+    echo ""
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+      echo "Enter the path to your shell's config file:"
+      read -p "Path: " -r
+      echo ""
+      if [[ ! -f $REPLY ]]; then
+        echo "Error: file not found"
+        exit 1
+      fi
+      if [[ ! -w $REPLY ]]; then
+        echo "Error: file not writable"
+        exit 1
+      fi
+      if [[ $(grep -c "export PATH=\$PATH:$HOME/.mtl/bin" $REPLY) -gt 0 ]]; then
+        echo "Mittel already added to PATH!"
+        echo "Mittel not added to PATH."
+        echo "You can add it manually by adding the following line to your shell's config file:"
+        echo "  export PATH=\$PATH:$HOME/.mtl/bin"
+        exit 1
+      fi
+      echo "export PATH=\$PATH:$HOME/.mtl/bin" >> $REPLY
+      echo "Mittel added to PATH!"
+    else
+      echo "Mittel not added to PATH."
+      echo "You can add it manually by adding the following line to your shell's config file:"
+      echo "  export PATH=\$PATH:$HOME/.mtl/bin"
+    fi
   fi
   echo "Mittel added to PATH!"
+else
+  echo "Mittel not added to PATH."
+  echo "You can add it manually by adding the following line to your .bashrc or .zshrc:"
+  echo "  export PATH=\$PATH:$HOME/.mtl/bin"
 fi
 
 echo "----------------------------------------"
